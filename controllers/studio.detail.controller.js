babf429d2168209e868b350f90c279f9ef0ff443
Rolland <rsadiraaksyana@gmail.com>
2023-10-05 13:48:30 +0700

    added homepage, booking page, studio details page

---
+++ controllers/studio.detail.controller.js
@@ -0,0 +1,7 @@
+class StudioDetailController {
+  async indexStudioDetail(req, res) {
+    res.render("studio-detail");
+  }
+}
+
+module.exports = StudioDetailController;
--- routers/router.js
+++ routers/router.js
@@ -3,6 +3,7 @@ const home = require("./home");
 const about = require("./about");
 const booking = require("./booking");
 const userLogin = require("./user");
+const studioDetail = require("./studio.detail");
 
 const router = express.Router();
 
@@ -11,5 +12,6 @@ router.use("/home", home);
 router.use("/", about);
 router.use("/", booking);
 router.use("/", userLogin);
+router.use("/", studioDetail);
 
 module.exports = router;
